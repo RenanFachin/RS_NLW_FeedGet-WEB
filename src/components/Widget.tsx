f8c4f97546aedeb50e71177d03a004d6f4745c3b
Renan <renan.fachin@hotmail.com>
2022-11-28 18:14:19 -0300

    Utilizando headlessui/react para adicionar acessibilidade ao popover do botão

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,24 +1,14 @@
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 import { ChatTeardropDots } from 'phosphor-react'
 
 export function Widget() {
-    // Criando um estado para controlar se o widget foi pressionado ou não. (Boolean)
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-    // Criando uma função para alterar a visibilidade do widget
-    function toogleWidgetVisibility(){
-        setIsWidgetOpen(!isWidgetOpen)
-    }
 
     return (
-        <div className='absolute bottom-5 right-5'>
+        <Popover className='absolute bottom-5 right-5'>
 
-            {/* Criando uma condicional a apartir do estado para mostrar ou não algo */}
-            { isWidgetOpen ? <p>Abriu</p> : null}
+            <Popover.Panel>Abriu</Popover.Panel>
 
-            <button 
-            onClick={toogleWidgetVisibility}
-            className='flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group'>
+            <Popover.Button className='flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group'>
                 <ChatTeardropDots className='w-6 h-6' />
 
                 {/* 
@@ -29,7 +19,7 @@ export function Widget() {
                     <span className='pl-2'></span>
                     FeedBack
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
