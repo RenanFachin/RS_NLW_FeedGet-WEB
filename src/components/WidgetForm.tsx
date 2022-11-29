ab0bc2fbacc38705efda33f23ba720b454e10bdf
Renan <renan.fachin@hotmail.com>
2022-11-29 10:26:24 -0300

    Adiciona tipagem e cria state para armazenar a alteração do componente

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from '../assets/bug.svg'
@@ -40,7 +41,13 @@ const feedbackTypes = {
  * Ou seja, será percorrido o vetor e teremos acesso a: chave - valor
  */ 
 
+// Tipando apenas as "chaves" do objeto, fazendo que não seja possível selecionar um tipo diferente das chaves definidas no objeto
+type FeedbackType = keyof typeof feedbackTypes
+
 export function WidgetForm(){
+    // Passando o FeedbackType como tipagem para o estado, fazendo com que só seja possível 3 valores (bug, idea ou other) além do null
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
@@ -52,7 +59,8 @@ export function WidgetForm(){
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
                 {
                     Object.entries(feedbackTypes).map(([key, value]) => {
                         // [key, value] é a desestruturação do "item"
@@ -60,6 +68,7 @@ export function WidgetForm(){
                             <button 
                                 key={key}
                                 type="button"
+                                onClick={() => setFeedbackType(key as FeedbackType)}
                                 className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
                             >
                                 <img 
@@ -75,6 +84,9 @@ export function WidgetForm(){
                     })
                 }
             </div>
+            ) : (
+                <p>funcionando</p>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito durante a NLW: <a className="underline underline-offset-1" href="https://github.com/RenanFachin/RS_NLW_FeedGet-WEB">Github do projeto</a>
