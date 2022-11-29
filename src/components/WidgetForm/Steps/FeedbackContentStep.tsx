95f6b8a2150984d5f285996064ec93b79f919ddd
Renan <renan.fachin@hotmail.com>
2022-11-29 15:20:15 -0300

    Capturando o que for digitado no textarea e desabilitando o botão enquanto o state de comentário estiver vazio

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react"
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenShotButton } from "../ScreenshotButton";
@@ -13,6 +13,15 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
     
     // Criando um state para armazenar a screenshot recebida pelo componente
     const [screenshot, setScreenshot] = useState<string | null>(null)
+
+    // Criando um state para armazenar o comentário do feedback
+    const [comment, setComment] = useState('')
+
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault()
+
+        console.log(screenshot, comment)
+    }
 
 
     const feedbackTypeInfo = feedbackTypes[feedbackType]
@@ -35,10 +44,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea 
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
                 />
 
                 <footer className="flex gap-2 mt-2">
@@ -54,7 +64,9 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
                     <button 
                     type="submit" 
-                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    // Desabilitando o botão enquanto não estiver nada digitado
+                    disabled={comment.length === 0 }
+                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
                         Enviar feedback
                     </button>
